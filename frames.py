b1221d7f9918016cb79e9fffcc373193bedfc97c
CaDiBob <nikolay.v.byankin@gmail.com>
2022-11-16 14:10:16 +0900

    Добвил цикл

--- frames.py
+++ frames.py
@@ -2,9 +2,11 @@ import os
 
 
 def get_frames():
+    frames = []
     folder = os.path.join('files', 'frames')
-    with open(os.path.join(folder, 'rocket_frame_1.txt'))as file:
-        rocket_frame_1 = file.read()
-    with open(os.path.join(folder, 'rocket_frame_2.txt'))as file:
-        rocket_frame_2 = file.read()
-    return rocket_frame_1, rocket_frame_2
+    files = sorted(os.listdir(folder))
+    for frame in files:
+        with open(os.path.join(folder, frame))as file:
+            rocket_frame = file.read()
+        frames.append(rocket_frame)
+    return frames
